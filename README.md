5ed1dde4cc8ba0eecbed559ed5898367ee1dbe6d
rebecca wood <rebeccaseverowood@gmail.com>
2023-02-25 10:01:58 +1300

    added the inquirer to make the readme file

---
+++ README.md
@@ -0,0 +1,33 @@
+# Title
+d
+
+## Description
+d
+
+## Contents
+[- Installation](#installation)
+[- Usage](#usage)
+[- Licence](#licence)
+[- Contributions](#contributions)
+[- Tests](#tests)
+
+## Installation
+d
+
+## Usage
+d
+
+## Licence
+Artistic license 2.0
+
+## Contributions
+
+
+## Tests
+undefined
+
+## Questions
+If you have any questions, please contact me through my email:
+d
+or go to my github:
+https://github.com/d
--- index.js
+++ index.js
@@ -1,30 +1,141 @@
 // generate README file using commandd-line application
 // include what the app is for, how to use the app, how to install it, how to report issues, and how to make contributions
+import inquirer from 'inquirer'
+import fs from 'fs'
 
-// When a user is prompted then a high-quality, professional README.md is generated with:
-
-// The title of my project
-// Sections entitled:
-
-// Description
-// Table of Contents
-// Installation
-// Usage
-// License
-// Contributing
-// Tests
-// Questions
-
-
+// this is the function of the file structure
+const generateReadme = (response) =>
+// gotta use the weird quotes
 // When a user enters the project title then it is displayed as the title of the README
-// When a user enters a description, installation instructions, usage information, contribution guidelines,
-//  and test instructions then this information is added to the sections of the README entitled Description, Installation, 
-//  Usage, Contributing, and Tests
+// When a user enters a description, installation instructions, usage information, contribution guidelines and test instructions then this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests, 
+    `# Title
+${response.title}
+
+## Description
+${response.description}
+
+## Contents
+[- Installation](#installation)
+[- Usage](#usage)
+[- Licence](#licence)
+[- Contributions](#contributions)
+[- Tests](#tests)
+
+## Installation
+${response.installation}
+
+## Usage
+${response.usage}
+
+## Licence
+${response.licence}
+
+## Contributions
+${response.contributions}
+
+## Tests
+${response.test}
+
+## Questions
+If you have any questions, please contact me through my email:
+${response.email}
+or go to my github:
+https://github.com/${response.github}`;
+
+//inquirer.prompt 
+//([{}])
+inquirer
+    .prompt([
+        {
+            // The title of my project
+            type: 'input',
+            message: 'Whats is your projects title?',
+            name: 'title'
+        },
+        {
+            // Description
+            type: 'input',
+            message: 'Whats a description for your project?',
+            name: 'description'
+        },
+        {
+            // Installation
+            type: 'input',
+            message: 'Add your installation instructions..',
+            name: 'installation'
+        },
+        {
+            // Usage
+            type: 'input',
+            message: 'Add your usage instructions',
+            name: 'usage'
+        },
+        {
+            // License
+            // When a user chooses a license for their application from a list of options
+            type: 'list',
+            message: 'Add your licence..',
+            name: 'licence',
+            choices: ['Apache license 2.0', 'Artistic license 2.0', 'Do What The F*ck You Want To Public License']
+        },
+        {
+            // Contributing
+            type: 'input',
+            message: 'Add your contributors',
+            name: 'contributions'
+        },
+        {
+            // Tests
+            type: 'input',
+            message: 'Add your test instructions',
+            name: 'test'
+        },
+        {
+            // Github username
+            type: 'input',
+            message: 'What is your github username?',
+            name: 'github'
+        },
+        {
+            // email
+            type: 'input',
+            message: 'What is your email?',
+            name: 'email'
+        }
+    ])
+    //promise to use a function on the response
+    .then((response) => {
+
+        const readme = generateReadme(response);
+        // if you dont already have a file, create a one with the following
+        fs.writeFile(
+            // this is the file to be created
+            `README.md`,
+            readme,
+            (err) => err ? console.error(err) : console.log('Success')
+        )
+    })
+
+
 // When a user chooses a license for their application from a list of options then a badge for that license is added 
 // near the top of the README and a notice is added to the section of the README entitled License that explains which 
 // license the application is covered under
+
+
 // When a user enters their GitHub username then this is added to the section of the README entitled Questions, 
 // with a link to their GitHub profile
-// When a user enters their email address then this is added to the section of the README entitled Questions, with 
-// instructions on how to reach them with additional questions
+
+
 // When a user clicks on the links in the Table of Contents then they are taken to the corresponding section of the README
+
+
+
+// licences:
+
+// apache [![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
+
+// do what the [![License: WTFPL](https://img.shields.io/badge/License-WTFPL-brightgreen.svg)](http://www.wtfpl.net/about/)
+
+// artistic licence [![License: Artistic-2.0](https://img.shields.io/badge/License-Artistic_2.0-0298c3.svg)](https://opensource.org/licenses/Artistic-2.0)
+
+// MIT [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
